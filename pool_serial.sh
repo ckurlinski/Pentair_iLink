e9c7dc0ebc2a127fa7650b78eab86c7dd1f9f422
ckurlinski <ckurlinski@me.com>
2017-06-18 14:33:12 -0400

    Added Tests

--- pool_serial.sh
+++ pool_serial.sh
@@ -1,58 +1,59 @@
 #!/bin/bash
 
-# Set Colors
-	_set_colors() {
-		reset=$(echo -en '\033[0m')
-		red=$(echo -en '\033[00;31m')
-		green=$(echo -en '\033[00;32m')
-		yellow=$(echo -en '\033[00;33m')
-		blue=$(echo -en '\033[00;34m')
-		magenta=$(echo -en '\033[00;35m')
-		purple=$(echo -en '\033[00;35m')
-		cyan=$(echo -en '\033[00;36m')
-		lightgray=$(echo -en '\033[00;37m')
-		lred=$(echo -en '\033[01;31m')
-		lgreen=$(echo -en '\033[01;32m')
-		lyellow=$(echo -en '\033[01;33m')
-		lblue=$(echo -en '\033[01;34m')
-		lmagenta=$(echo -en '\033[01;35m')
-		lpurple=$(echo -en '\033[01;35m')
-		lcyan=$(echo -en '\033[01;36m')
-		white=$(echo -en '\033[01;37m')
-	}
-# Set Print formatting
-	## Header
-		_header() {
-			printf "\n${lcyan}==========  %s  ==========${reset}\n" "$@"
+# Set Terminal Colors and Formating
+	## Set Colors
+		_set_colors() {
+			reset=$(echo -en '\033[0m')
+			red=$(echo -en '\033[00;31m')
+			green=$(echo -en '\033[00;32m')
+			yellow=$(echo -en '\033[00;33m')
+			blue=$(echo -en '\033[00;34m')
+			magenta=$(echo -en '\033[00;35m')
+			purple=$(echo -en '\033[00;35m')
+			cyan=$(echo -en '\033[00;36m')
+			lightgray=$(echo -en '\033[00;37m')
+			lred=$(echo -en '\033[01;31m')
+			lgreen=$(echo -en '\033[01;32m')
+			lyellow=$(echo -en '\033[01;33m')
+			lblue=$(echo -en '\033[01;34m')
+			lmagenta=$(echo -en '\033[01;35m')
+			lpurple=$(echo -en '\033[01;35m')
+			lcyan=$(echo -en '\033[01;36m')
+			white=$(echo -en '\033[01;37m')
 		}
-	## Menu selection
-		_select() {
-			printf "${lgreen}%s${reset}\n" "$@"
-		}
-	## Seperator
-		_sep() {
-			printf "\n${lpurple}========================================${reset}\n" "$@"
-		}
-	## Successful Command
-		_success() {
-			printf "${green}✔ %s${reset}\n" "$@"
-		}
-	## Error
-		_error() {
-			printf "${red}✖ %s${reset}\n" "$@"
-		}
-	## Successful Removal
-		_removed() {
-			printf "${green}✖ %s${reset}\n" "$@"
-		}
-	## Warning
-		_warning() {
-			printf "${yellow}➜ %s${reset}\n" "$@"
-		}
-	## Note
-		_note() {
-			printf "${lyellow}Note:${reset}  ${lyellow}%s${reset}\n" "$@"
-		}
+	## Set Print formatting
+		### Header
+			_header() {
+				printf "\n${lcyan}==========  %s  ==========${reset}\n" "$@"
+			}
+		### Menu selection
+			_select() {
+				printf "${lgreen}%s${reset}\n" "$@"
+			}
+		### Seperator
+			_sep() {
+				printf "\n${lpurple}========================================${reset}\n" "$@"
+			}
+		### Successful Command
+			_success() {
+				printf "${green}✔ %s${reset}\n" "$@"
+			}
+		### Error
+			_error() {
+				printf "${red}✖ %s${reset}\n" "$@"
+			}
+		### Successful Removal
+			_removed() {
+				printf "${green}✖ %s${reset}\n" "$@"
+			}
+		### Warning
+			_warning() {
+				printf "${yellow}➜ %s${reset}\n" "$@"
+			}
+		### Note
+			_note() {
+				printf "${lyellow}Note:${reset}  ${lyellow}%s${reset}\n" "$@"
+			}
 # Menu List Function
 	menu_list_template() {
 		count=1
@@ -92,8 +93,22 @@
 		done
 	}
 # Command Line Parameters
-  _serial_dev="/dev/tty.usbserial-A501JX6M"
-  _serial_cmd="socat - ${_serial_dev},raw,echo=0,crnl"
+	## Serial Device
+  	_serial_dev="/dev/tty.usbserial-A501JX6M"
+		if [[ -e ${_serial_dev} ]]; then
+			echo "Using serial device : ${_serial_dev}"
+		else
+			echo "Serial Device not installed, Goodbye"
+			exit
+		fi
+	## Socat
+		if [[ -e `(which socat)` ]]; then
+			echo "Using socat at: `(which socat)`"
+		else
+			echo "socat not installed, Goodbye"
+			exit
+		fi
+	  _serial_cmd="socat - ${_serial_dev},raw,echo=0,crnl"
 # Serial Command
   _SERIAL_CMD_FN() {
     echo ${_PENTAIR_CMD} | ${_serial_cmd}
