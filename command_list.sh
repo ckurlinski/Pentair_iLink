bdffd0a6b9252210f4d6d7764bd3decd9b59db02
ckurlinski <ckurlinski@me.com>
2017-06-20 11:29:44 -0400

    Tests

--- command_list.sh
+++ command_list.sh
@@ -3,28 +3,30 @@
 # COSMSGS
 # Query
 _COSMSGS_QUERY_FN() {
-	_MESSAGE="System Messages setting is - "
+  _sep
+	_MESSAGE="System Messages Settings"
 	_PENTAIR_CMD="#COSMSGS ?<cr>"
 	_SERIAL_CMD_FN
+  _sep
 }
 # UNITS
 ## QUERY
 _UNITS_QUERY_FN() {
-	_MESSAGE="Current Temperature Units is - "
+	_MESSAGE="Current Temperature Units"
 	_PENTAIR_CMD="#UNITS ?<cr>"
 	_SERIAL_CMD_FN
 }
 # AIRTMP
 ## Query
 _AIRTMP_QUERY_FN() {
-	_MESSAGE="Current Air Temperature Units is - "
+	_MESSAGE="Current Air Temperature"
 	_PENTAIR_CMD="#AIRTMP ?<cr>"
 	_SERIAL_CMD_FN
 }
 # POOLTMP
 ## Query
 _POOLTMP_QUERY_FN() {
-	_MESSAGE="Current Pool Temperature is - "
+	_MESSAGE="Current Pool Temperature"
 	_PENTAIR_CMD="#POOLTMP ?<cr>"
 	_SERIAL_CMD_FN
 }
